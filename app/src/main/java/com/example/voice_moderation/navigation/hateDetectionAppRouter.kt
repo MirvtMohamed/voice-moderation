d238dc2ad626aefb302a990110118501ee499963
Nada Salah <nada1582003@gmail.com>
2025-04-09 16:49:17 +0200

    authantication-ui

---
+++ app/src/main/java/com/example/voice_moderation/navigation/hateDetectionAppRouter.kt
@@ -0,0 +1,20 @@
+package com.example.voice_moderation.navigation
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+sealed class Screen {
+    object SignUp : Screen()
+    object TermsAndConditionsScreen : Screen()
+    object LoginScreen : Screen()
+}
+
+object HateDetectionAppRouter {
+
+
+    var currentScreen: MutableState<Screen> = mutableStateOf(Screen.SignUp)
+
+    fun navigateTo(destination: Screen) {
+        currentScreen.value = destination // Update the current screen
+    }
+}
